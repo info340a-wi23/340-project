780d7479953d333a7564fb6385b8c2c745995132
soniadashti <sodashti@uw.edu>
2023-02-23 22:47:02 -0800

    Fixed styling

--- src/dashboard.js
+++ src/dashboard.js
@@ -2,7 +2,7 @@
   <main>
     <div className="dashboard">
       <h1>View Your Job Stats</h1>
-      <img className="data-views" src="websiteimg/dashboard.png" alt="Table and bar chart of user data" />
+      <img className="data-views" src="../../websiteimg/dashboard.png" alt="Table and bar chart of user data" />
     </div>
   </main>
   <div>
