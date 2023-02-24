9f0e9611b7bfa2ed4b3ad97b201a7b91e1ae48bd
emilyghale <ehale1@uw.edu>
2023-02-24 11:50:55 -0800

    add filter

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function Dashboard() {
   const location = useLocation();
   const jobs = location.state?.jobs || [];
 
+  const [statusFilter, setStatusFilter] = useState('All');
+
+  const handleFilterChange = (event) => {
+    setStatusFilter(event.target.value);
+  };
+
+  const filteredJobs = statusFilter === 'All' ? jobs : jobs.filter(job => job.status === statusFilter);
+
   return (
     <div>
       <header>
         <h1 className="text-center">Dashboard</h1>
+        <div className="filter-container">
+          <label htmlFor="status-filter">Filter by Status:</label>
+          <select id="status-filter" onChange={handleFilterChange} value={statusFilter}>
+            <option value="All">All</option>
+            <option value="Applied">Applied</option>
+            <option value="Interview">Interview</option>
+            <option value="Offer Pending">Offer Pending</option>
+            <option value="Rejected">Rejected</option>
+          </select>
+          <button onClick={() => setStatusFilter('All')}>Clear Filter</button>
+        </div>
       </header>
       <main>
         <table>
@@ -22,7 +41,7 @@ export default function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            {jobs.map((job, index) => (
+            {filteredJobs.map((job, index) => (
               <tr key={index}>
                 <td>{job.date}</td>
                 <td>{job.title}</td>
