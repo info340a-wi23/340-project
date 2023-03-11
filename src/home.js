edfc86a48c199ac3f1850e39c64b5764ed668016
soniadashti <sodashti@uw.edu>
2023-03-11 14:08:37 -0800

    Indent

--- src/home.js
+++ src/home.js
@@ -11,10 +11,10 @@ export default function Home() {
             <p>Stay organized in your job hunt</p>
           </header>
           <div className="description">
-          <p> Job Trackr is a career service tool that supports users during their job search. Users can upload their resume
-            and use a checklist to see what entities they need on their resume and are provided with a resume template that
-            helps them improve their resume. Users can also add jobs they have applied to and their application status to help them keep track of applied jobs. 
-          </p>
+            <p> Job Trackr is a career service tool that supports users during their job search. Users can upload their resume
+              and use a checklist to see what entities they need on their resume and are provided with a resume template that
+              helps them improve their resume. Users can also add jobs they have applied to and their application status to help them keep track of applied jobs. 
+            </p>
           </div>
           <Link to="/add-job">
             <button type="button" className="btn btn-light btn-lg">Go to Job Log</button>
