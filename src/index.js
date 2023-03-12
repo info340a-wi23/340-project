76c997aa4dcb655ad27fe80089e913b67d312d50
andrewle7 <ale527@uw.edu>
2023-03-11 23:09:35 -0800

    firebase added in code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcfERaRBbc6WKaKt4f_ZlLTiPdyD4QSOU",
+  authDomain: "jobtrackr-d910d.firebaseapp.com",
+  projectId: "jobtrackr-d910d",
+  storageBucket: "jobtrackr-d910d.appspot.com",
+  messagingSenderId: "259048737997",
+  appId: "1:259048737997:web:a3acee881c44c67ae06d84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
