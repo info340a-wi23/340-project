9e459c6ac86e66ec53bf29b4094521cab57b680b
Tyrell Garza <tyrellgarza@t-MBP-14.local>
2023-03-13 11:11:18 -0700

    conditional profile buton

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,9 +13,9 @@ function NavBar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        
-        <Link to="/" className="navbar-brand" href="index.html"><img src="/favicon.png" alt="Logo" width={28} height={28} 
-        className="d-inline-block align-text-top" /> JobTrackr</Link>
+
+        <Link to="/" className="navbar-brand" href="index.html"><img src="/favicon.png" alt="Logo" width={28} height={28}
+          className="d-inline-block align-text-top" /> JobTrackr</Link>
 
         <button
           className="navbar-toggler"
@@ -33,53 +33,59 @@ function NavBar(props) {
           className={`collapse navbar-collapse ${click ? 'show' : ''}`}
           id="navbarNav"
         >
-        <ul className="navbar-nav me-auto" style={{ display: "flex" }}>
-          <li className="nav-item" style={{ display: "inline-block" }}>
-            <Link
-              to="/home"
-              className="nav-link nav-button"
-              onClick={handleClick}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item" style={{ display: "inline-block" }}>
-            <Link
-              to="/checklist"
-              className="nav-link nav-button"
-              onClick={handleClick}
-            >
-              Checklist
-            </Link>
-          </li>
-          <li className="nav-item" style={{ display: "inline-block" }}>
-            <Link
-              to="/add-job"
-              className="nav-link nav-button"
-              onClick={handleClick}
-            >
-              Job Log
-            </Link>
-          </li>
-          <li className="nav-item" style={{ display: "inline-block" }}>
-            <Link to="/about" className="nav-link nav-button" onClick={handleClick}>
-              About
-            </Link>
-          </li>
-        </ul>
+          <ul className="navbar-nav me-auto" style={{ display: "flex" }}>
+            <li className="nav-item" style={{ display: "inline-block" }}>
+              <Link
+                to="/home"
+                className="nav-link nav-button"
+                onClick={handleClick}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item" style={{ display: "inline-block" }}>
+              <Link
+                to="/checklist"
+                className="nav-link nav-button"
+                onClick={handleClick}
+              >
+                Checklist
+              </Link>
+            </li>
+            <li className="nav-item" style={{ display: "inline-block" }}>
+              <Link
+                to="/add-job"
+                className="nav-link nav-button"
+                onClick={handleClick}
+              >
+                Job Log
+              </Link>
+            </li>
+            <li className="nav-item" style={{ display: "inline-block" }}>
+              <Link to="/about" className="nav-link nav-button" onClick={handleClick}>
+                About
+              </Link>
+            </li>
+            {currentUser && (
+              <li className="nav-item" style={{ display: "inline-block" }}>
+                <Link to="/profile" className="nav-link nav-button" onClick={handleClick}>
+                  Profile
+                </Link>
+              </li>
+            )}
+          </ul>
         </div>
         <div className="login-navbar">
-        {currentUser ? (
-          <button className="btn btn-light btn-sm" 
-          onClick={handleSignOut}>Sign Out</button> )
-          : (
-          <button className="btn btn-light btn-sm" style={{ display: "inline-block" }}> 
-          <a href="/login" className="login">Login</a></button>
-          )}
-          </div>
+          {currentUser ? (
+            <button className="btn btn-light btn-sm"
+              onClick={handleSignOut}>Sign Out</button>)
+            : (
+              <button className="btn btn-light btn-sm" style={{ display: "inline-block" }}>
+                <a href="/login" className="login">Login</a></button>
+            )}
+        </div>
       </div>
     </nav>
   );
 }
-
 export default NavBar;
