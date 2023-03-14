2bb28189c1731fc3b1ef12bdd309a74d2bf4d6a6
andrewle7 <ale527@uw.edu>
2023-03-13 19:22:03 -0700

    profile form code gone

--- src/components/profile.js
+++ src/components/profile.js
@@ -52,17 +52,12 @@ export default function Profile(props) {
       <div className='card'>
         <div className="profile">
           <p className="profile-welcome">Welcome {props.currentUser.userName && props.currentUser.userName}!</p>
-          {isEditing ? (
-            <ProfileForm currentUser={props.currentUser} onSave={handleSave} />
-          ) : (
             <div className="profile-info">
                 <p>Total Jobs Applied To: {jobsAppliedToCount}</p>
                 <p>Job Interviews: {jobsInterviewCount}</p>
                 <p>Job Offer Pendings: {jobsOfferPendingCount}</p>
                 <p>Job Rejections: {jobsRejectedCount}</p>
-              <button type="button" className="btn btn-light btn-lg" onClick={() => setIsEditing(true)}>Edit Profile</button>
             </div>
-          )}
             <div className="dashboard-link">
               <p>Dashboard</p>
               <Link to="/dashboard">
