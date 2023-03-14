b83aa4aa744df36360a39f30a5aa73d2f7eb6ead
soniadashti <sodashti@uw.edu>
2023-03-13 17:30:35 -0700

    Login header styling

--- src/components/login.js
+++ src/components/login.js
@@ -30,7 +30,7 @@ if (currentUser.userName) { //if signed in
 }
   return (
     <div className="login">
-      <header>
+      <header className="login-header">
         <h1>Log in to continue</h1>
       </header>
       <p>By signing in you can access your dashboard</p>
